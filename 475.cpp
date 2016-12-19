63273534f5f1a42dcac33411915ed9f980f8ec57
Bright1992 <wangbowenmax@163.com>
2016-12-19 23:29:22 +0800

    init

---
+++ 475.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define INF 0x7FFFFFFF
+
+int glo = 0;
+
+int findRadius(vector<int> &houses, vector<int> &heaters) {
+	sort(houses.begin(), houses.end());
+	sort(heaters.begin(), heaters.end());
+	int s1 = houses.size(), s2 = heaters.size();
+	int p = 0;
+	int radius = 0;
+	int rp = 0;
+	for (int i = 0; i < s1; ++i) {
+		while (p<s2&&houses[i] > heaters[p])	p++;
+		if (p == 0)	rp = heaters[p] - houses[i];
+		else if (p < s2) rp = min(heaters[p] - houses[i], houses[i] - heaters[p - 1]);
+		else rp = houses[i] - heaters[p - 1];
+		radius = max(radius, rp);
+	}
+	return radius;
+}
+
+int main() {
+	vector<int> houses = { 25921153, 510616708 }, heaters = { 771515668, 357571490, 44788124, 927702196, 952509530 };
+	glo = glo + houses.size();
+	cout << findRadius(houses,heaters) << endl;
+	system("Pause");
+	return 0;
+}
---
+++ 6.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string convert(string text, int nRows) {
+	string ret = "";
+	int s = text.size();
+	int T = nRows * 2 - 2;
+	if (nRows == 1)	return text;
+	if (s == 0)	return "";
+	int n = (s-1) / T, r = (s-1)%T+1;
+	vector<int> q = vector<int>(nRows,0);
+	for (int i = 0; i < nRows; ++i) {
+		if (i == 0 || i == nRows - 1) {
+			q[i] = n;
+			if (i == 0)	q[i] = q[i] + 1;
+			if (i == nRows - 1 && r >= nRows)	q[i] = q[i] + 1;
+		}
+		else {
+			q[i] = 2 * n;
+			if (r > i)	q[i] = q[i] + 1;
+			if (r >= 2 * nRows - i - 1)	q[i] = q[i] + 1;
+		}
+	}
+	for (int i = 0; i < nRows - 1; ++i)
+		q[i + 1] = q[i] + q[i + 1];
+	q.insert(q.begin(), 0);
+	ret = string(s, '0');
+	for (int i = 0; i < s; ++i) {
+		int pr = i%T+1, pn = i / T;
+		if (pr != 1 && pr != nRows)	pn *= 2;
+		if (pr <= nRows)	ret[q[pr - 1] + pn] = text[i];
+		else	ret[q[2 * nRows - pr - 1] + pn + 1] = text[i];
+	}
+	return ret;
+}
+
+int main() {
+	cout<<convert("abcdefghijklmnopqrstuvwxyz",1)<<endl;
+	system("Pause");
+	return 0;
+}
